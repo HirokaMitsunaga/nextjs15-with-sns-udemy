b0c40c83a71c63e63e0d16de57a7c1738551b741
hiroka <mitsunagahrk87@gmail.com>
2025-11-30 16:36:18 +0900

    useFormStatusの実装

---
+++ components/component/SubmmitButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { SendIcon } from "./Icons";
+import { useFormStatus } from "react-dom";
+
+export const SubmmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      <Button variant="ghost" size="icon" disabled={pending}>
+        <SendIcon className="h-5 w-5 text-muted-foreground" />
+        <span className="sr-only">Tweet</span>
+      </Button>
+    </>
+  );
+};
