c1b528495f382334a97dfa5fb1cfc9a50f8e0772
hiroka <mitsunagahrk87@gmail.com>
2025-11-30 15:16:20 +0900

    サーバーサイドの処理を別ファイルへ移動

---
+++ lib/actions.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { z } from "zod";
+import { prisma } from "./prisma";
+
+export async function addPostAction(formData: FormData) {
+  const { userId } = auth();
+
+  try {
+    const postText = formData.get("post") as string;
+    const postTextSchema = z
+      .string()
+      .min(1, "ポスト内容を入力してください")
+      .max(140, "140字以内で入力してください");
+
+    const validatePostText = postTextSchema.parse(postText);
+    await prisma.post.create({
+      data: {
+        content: validatePostText,
+        authorId: userId!,
+      },
+    });
+    return {
+      success: true,
+      error: undefined,
+    };
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return {
+        error: error.errors.map((e) => e.message).join(", "),
+        success: false,
+      };
+    } else if (error instanceof Error) {
+      return {
+        error: error.message,
+        success: false,
+      };
+    } else {
+      return {
+        error: "予期せぬエラーが発生しました",
+        success: false,
+      };
+    }
+  }
+}
