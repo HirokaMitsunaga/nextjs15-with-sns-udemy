7f7278e2cb8ec05e91c44a27250924c7b997d4cd
hiroka <mitsunagahrk87@gmail.com>
2025-12-04 09:39:35 +0900

    29. ServerActionsでいいね機能を実装しよう

--- components/component/PostInteraction.tsx
+++ components/component/PostInteraction.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { HeartIcon, MessageCircleIcon, Share2Icon } from "./Icons";
+import { prisma } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 type PostInteractionProps = {
   postId: string;
@@ -13,11 +16,46 @@ export const PostInteraction = ({
   initialLikes,
   commentNumber,
 }: PostInteractionProps) => {
+  const likeAction = async () => {
+    "use server";
+    const { userId } = auth();
+    if (!userId) {
+      throw new Error("ユーザーは認証されていません");
+    }
+    try {
+      const exisitingLike = await prisma.like.findFirst({
+        where: {
+          postId,
+          userId,
+        },
+      });
+      if (exisitingLike) {
+        await prisma.like.delete({
+          where: {
+            id: exisitingLike.id,
+          },
+        });
+        revalidatePath("/");
+      } else {
+        await prisma.like.create({
+          data: {
+            postId,
+            userId,
+          },
+        });
+        revalidatePath("/");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="flex items-center">
-      <Button variant="ghost" size="icon">
-        <HeartIcon className="h-5 w-5 text-muted-foreground" />
-      </Button>
+      <form action={likeAction}>
+        <Button variant="ghost" size="icon">
+          <HeartIcon className="h-5 w-5 text-muted-foreground" />
+        </Button>
+      </form>
       <span className="-m1-1">{initialLikes.length}</span>
       <Button variant="ghost" size="icon">
         <MessageCircleIcon className="h-5 w-5 text-muted-foreground" />
